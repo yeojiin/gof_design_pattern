57cfc2613845d8cf22c6cf6bbe1237e6b699c5f0
jylim <jylim@12cm.co.kr>
2023-11-20 21:54:38 +0900

    test(Duck): Duck 테스트 케이스 작성

--- src/test/java/com/study/pattern/DesignPatternApplicationTests.java
+++ src/test/java/com/study/pattern/DesignPatternApplicationTests.java
@@ -1,4 +1,4 @@
-package com.study.design_pattern;
+package com.study.pattern;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/com/study/pattern/domain/DuckTest.java
@@ -0,0 +1,45 @@
+package com.study.pattern.domain;
+
+import com.study.pattern.strategy.domain.duck.Duck;
+import com.study.pattern.strategy.domain.duck.ReadheadDuck;
+import com.study.pattern.strategy.domain.duck.RubberDuck;
+import com.study.pattern.strategy.domain.quack.MuteQuack;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("전략 패턴관련 테스트")
+public class DuckTest {
+
+    Duck readheadDuck = new ReadheadDuck();
+    Duck rubberDuck = new RubberDuck();
+
+    @Test
+    @DisplayName("고무 오리는 날지 못하고, 삑삑 거린다.")
+    void createRubberDuckTest() {
+        performDuck(rubberDuck);
+    }
+
+    @Test
+    @DisplayName("붉은머리 오리는 날 수 있고, 꽥꽥 거린다.")
+    void createRedheadDuckTest() {
+        performDuck(readheadDuck);
+    }
+
+    @Test
+    @DisplayName("붉은머리 오리가 시끄러워 조용히 시켰다.")
+    void muteRubberDuck() {
+        performDuck(readheadDuck);
+        rubberDuck.changeQuackBehavior(new MuteQuack());
+        rubberDuck.performQuack();
+    }
+
+    /**
+     * 공통 행동 호출
+     * @param duck
+     */
+    private static void performDuck(Duck duck) {
+        duck.display();
+        duck.performFly();
+        duck.performQuack();
+    }
+}
